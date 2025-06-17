62f054d3afa4ac4e5bfb8edd9eb43def1c77db6b
Ranjit1818 <ranjeetkulkarni18@gmail.com>
2025-06-18 01:26:09 +0530

    Your meaningful commit message

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 // src/main.jsx
+import React from "react";
 
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
@@ -12,4 +13,3 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     </BrowserRouter>
   </React.StrictMode>
 );
-
